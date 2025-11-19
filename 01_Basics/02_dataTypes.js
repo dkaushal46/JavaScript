afa32244669f0cdd96adfde6c48a026c7070789d
Dikshansh Kaushal <dikshanshnath@gmail.com>
2025-11-19 11:07:50 +0530

    updated

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -1,3 +1,25 @@
 "use strict"; //Treat all code to newer version
 
 // alert(3+3) //we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("DDDDD");
+
+
+let name = "DDDDDDDD"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//big int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof isLoggedIn);
+
+console. log(typeof undefined); // undefined
+console. log(typeof null); // object
