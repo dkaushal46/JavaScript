5e100f488c694fa746d86542e938f44c208c8540
Dikshansh Kaushal <dikshanshnath@gmail.com>
2025-11-19 13:53:03 +0530

    updated

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score1 ="33ewr"
+let score2 = 33
+
+
+console.log(typeof score1)
+console.log(typeof score2)
+
+let converted = Number(score1)
+console.log(typeof converted)
+console.log(converted)
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
